6afe6f16a977ec2c9e4402dbaad3496434682ccc
luckygro <lukas@luckyg.de>
2019-01-19 01:45:38 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Link } from 'react-router-dom'
+
+import logo from './images/logo.png';
+import eyecatcher from './images/schoolchildren.jpg'
+
 import './App.css';
+
+var eyecatcherBackground = {
+  backgroundImage: "url(" + eyecatcher + ")"
+};
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <header className="HeroImage" style={eyecatcherBackground}>
+          <div className="Container">
+            <div className="LogoImage" >
+              <img src={logo} ></img>
+            </div>
+          </div>
         </header>
+        <section className="About">
+          <div className="Container">
+            <h1>Willkommen auf der Website des Christlichen Kinderhilfswerk Luwero - Uganda</h1>
+            <p>Unsere neue Website befindet sich gerade im Aufbau. Wir bitten Sie um Verständnis!</p>
+            <a href="http://www.luwero-community-projects.de" >Informationen auf unserer bisherigen Website</a>
+          </div>    
+        </section>
+
       </div>
     );
   }
 }
 
 export default App;
+
+
